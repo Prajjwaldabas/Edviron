2422e64884a8efb095b3b40babf450323362ded1
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-10-12 19:05:39 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
  
    <div className="App">
       <Routes>
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={< Login/>} />
         <Route path="/signup" element={< SignUp/>} />
       </Routes>
